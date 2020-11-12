d4bed39e0b6f33fc3a13507bba113a95fd8971ac
JACKSON GERBES <jg0244@students.necsd.net>
2020-11-12 15:29:36 +0000

    Jackson Gerbes :D

--- Chapter2Test.java
+++ Chapter2Test.java
@@ -1,7 +1,15 @@
-
+import java.util.Scanner;
 
 public class Chapter2Test {
   public static void main(String[] args) {
-  
-  }
+  Scanner in = new Scanner(System.in);
+   
+  System.out.print("Input first number: ");
+  int num1 = in.nextInt();
+   
+  System.out.print("Input second number: ");
+  int num2 = in.nextInt();
+   System.out.println("Average of the numbers is: " + 
+   (num1 + num2) / 2);
+ }
 }
